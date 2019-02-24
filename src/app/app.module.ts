792080a4aeb9b0febb9751d1cb2412e8bdc889e0
pirumpi <pirumpi@gmail.com>
2019-02-23 23:15:22 -0700

    UnitTest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AvatarComponent } from './avatar/avatar.component';
+import { HeaderComponent } from './header/header.component';
+import { BoardComponent } from './board/board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AvatarComponent,
+    HeaderComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
